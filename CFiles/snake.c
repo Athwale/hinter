902161cf8776d54509933b08008d002e7b7a36ae
Ondrej Mejzlik <omejzlik@redhat.com>
2025-12-09 18:51:18 +0100

    ncurses example

---
+++ CFiles/snake.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ncurses.h>
+
+int main(void) {
+
+    initscr();
+    cbreak();
+    noecho();
+
+    mvaddch(0, 0, '+');
+    mvaddch(LINES - 1, 0, '-');
+    mvaddstr(10, 30, "press any key to quit");
+    refresh();
+
+    getch();
+
+    endwin();
+
+    return 0;
+}

e1b37b975968af500f42a1ddfd246abc5fb691c0
Ondrej Mejzlik <omejzlik@redhat.com>
2025-12-13 13:18:44 +0100

    Update drawing funct

--- CFiles/snake.c
+++ CFiles/snake.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <ncurses.h>
+#include <math.h>
 
-#define SIZE_X 20
-#define SIZE_Y 20
+// todo check sqrt and add if needed.
+#define SIZE 400
+#define EMPTY 0
+#define HEAD 1
+#define BODY 2
+#define FOOD 3
 
 struct block {
     int posX;
@@ -12,69 +17,78 @@ struct block {
     struct block *next;
 };
 
-void print_field(struct block arr[SIZE_X][SIZE_Y]) {
+void print_field(struct block arr[SIZE]) {
     char c = '\0';
-    for (int i = 1; i < SIZE_Y; i++) {
-        for (int j = 1; j < SIZE_X; j++) {
-            switch (arr[i][j].type) {
-                case 0:
-                    // Empty
-                    c = '-';
-                    break;
-                case 1:
-                    // Food
-                    c = '*';
-                    break;
-                case 2:
-                    // Body
-                    c = '#';
-                    break;
-                case 3:
-                    // Head
-                    c = 'O';
-                    break;
-            }
-            mvaddch(arr[i][j].posX, arr[i][j].posY, c);
+    for (int i = 0; i < SIZE; i++) {
+        switch (arr[i].type) {
+            case EMPTY:
+                c = '-';
+                break;
+            case FOOD:
+                c = '*';
+                break;
+            case BODY:
+                c = '#';
+                break;
+            case HEAD:
+                c = '0';
+                break;
         }
-    }
+        mvaddch(arr[i].posX, arr[i].posY, c);
+        }
     refresh();
 }
 
-void update_part(struct block element, char direction) {
-    switch (direction) {
-
-    }
-}
-
-void update_field(struct block arr[SIZE_X][SIZE_Y], char key) {
-    for (int i = 0; i < SIZE_Y; i++) {
-        for (int j = 0; j < SIZE_X; j++) {
-            if (arr[i][j].type == 3 || arr[i][j].type == 2) {
-                // Update the head.
-                update_part(arr[i][j], key);
-            }
-        }
-    }
-}
-
 int main(void) {
-    // todo switch to pointers for practice?
-    // todo make a pointer linked list
+    // todo make a pointer linked field
 
     // Init playing field.
-    struct block field[SIZE_X][SIZE_Y];
+    struct block field[SIZE];
 
-    for (int i = 1; i < SIZE_Y; i++) {
-        for (int j = 1; j < SIZE_X; j++) {
-            field[i][j].posX = i;
-            field[i][j].posY = j;
-            field[i][j].type = 0;
+    // Init linked list.
+    int width = (int)sqrt(SIZE);
+    int x = 0;
+    int y = 0;
+
+    for (int i = 0; i < SIZE; i++) {
+        if (i == 0) {
+            field[i].prev = nullptr;
+            field[i].next = &field[i+1];
+        } else if (i == SIZE - 1) {
+            field[i].next = nullptr;
+            field[i].prev = &field[i-1];
+        } else {
+            field[i].prev = &field[i-1];
+            field[i].next = &field[i+1];
         }
+        // All blocks are initially empty space.
+        field[i].type = EMPTY;
+
+        if (x == width) {
+            y++;
+            x = 0;
+        }
+
+        field[i].posX = x;
+        field[i].posY = y;
+        x++;
     }
-    // Init snake.
-    field[10][5].type = 2;
-    field[10][6].type = 2;
-    field[10][7].type = 3;
+
+    // Init snake
+    struct block *snake = &field[150];
+    snake->type = HEAD;
+    snake->prev->type = BODY;
+    snake->prev->prev->type = BODY;
+    snake->prev->prev->prev->type = BODY;
+
+    struct block *block = &field[0];
+    // We have a pointer to a struct so we need ->
+    puts("Walk through the field:");
+    while (block->next != NULL) {
+        printf("%d, Ptr: %p, Prev: %p, Next: %p, X: %d, Y: %d \n", block->type, block, block->prev, block->next, block->posX, block->posY);
+        block = block->next;
+    }
+    printf("%d, Ptr: %p, Prev: %p, Next: %p, X: %d, Y: %d \n", block->type, block, block->prev, block->next, block->posX, block->posY);
 
     initscr();
     cbreak();
@@ -86,7 +100,7 @@ int main(void) {
     while (c != 'q') {
         scanf("%c", &c);
         // todo update field and redraw.
-        update_field(field, c);
+        //update_field(field, c);
         print_field(field);
     }
 

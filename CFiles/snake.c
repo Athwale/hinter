cafacc8f523cb1b631d9b52976e65e39a0cbdce6
Ondrej Mejzlik <omejzlik@redhat.com>
2025-12-15 08:58:17 +0100

    Tail following

--- CFiles/snake.c
+++ CFiles/snake.c
@@ -3,158 +3,129 @@
 #include <math.h>
 
 // todo check sqrt and add if needed.
-#define SIZE 400
-#define HEAD_POS 150
+#define SIZE 40
+#define HEAD_POS 20
+#define WIN_LENGTH 10
+#define START_LENGTH 3
 
 // block types:
-#define EMPTY 0
-#define HEAD 1
-#define BODY 2
-#define FOOD 3
+#define EMPTY '-'
+#define BODY '$'
+#define FOOD '*'
 
-//yyyyyy
-//x
-//x
-//x
+//  y y y
+// x
+// x
+// x
 
 struct block {
     int posX;
     int posY;
-    int type; // 0 - empty, 1 - food, 2 - body, 3 - head
-    struct block *prev;
-    struct block *next;
-    struct block *next_tail;
+    char type;
 };
 
-void print_field(struct block arr[SIZE]) {
-    char c = '\0';
+void print_field(char arr[SIZE][SIZE]) {
     for (int i = 0; i < SIZE; i++) {
-        switch (arr[i].type) {
-            case EMPTY:
-                c = '-';
-                break;
-            case FOOD:
-                c = '*';
-                break;
-            case BODY:
-                c = '#';
-                break;
-            case HEAD:
-                c = '0';
-                break;
-            default:
-                break;
+        for (int j = 0; j < SIZE; j++) {
+            mvaddch(i, j, arr[i][j]);
         }
-        mvaddch(arr[i].posX, arr[i].posY, c);
-        }
+    }
     refresh();
 }
 
-struct block *update_field(struct block *head, char direction) {
-    struct block *new_head = nullptr;
-    struct block *last_tail = head;
-    while (last_tail->next_tail != nullptr) {
-        last_tail = last_tail->next_tail;
+void process_move(struct block snake[], char direction, int length) {
+    // Update the snake array with new coordinates.
+    // todo implement boundaries.
+    // Take first element, back up coords, move it, put original coord to next element.
+    int prev_x = 0;
+    int prev_y = 0;
+
+    for (int i = 0; i < length; i++) {
+        if (i == 0) {
+            prev_x = snake[i].posX;
+            prev_y = snake[i].posY;
+            switch (direction) {
+                case 'w':
+                    snake[i].posX -= 1;
+                    break;
+                case 's':
+                    snake[i].posX += 1;
+                    break;
+                case 'a':
+                    snake[i].posY -= 1;
+                    break;
+                case 'd':
+                    snake[i].posY += 1;
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            int current_x = snake[i].posX;
+            int current_y = snake[i].posY;
+            snake[i].posX = prev_x;
+            snake[i].posY = prev_y;
+            prev_x = current_x;
+            prev_y = current_y;
+        }
+    }
+}
+
+void update_field(char arr[SIZE][SIZE], struct block snake[], int length) {
+    int x = 0;
+    int y = 0;
+    char type = '\0';
+
+    // Zero the whole field.
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            arr[i][j] = EMPTY;
+        }
     }
 
-    switch (direction) {
-        case 'w':
-            // move the head up and shift body positions.
-            head->type = BODY;
-            head->prev->type = HEAD;
-            new_head = head->prev;
-            break;
-        case 's':
-            // Move the head down and shift body positions.
-            // Head becomes body and the next block becomes the new head.
-            head->type = BODY;
-            head->next->type = HEAD;
-            new_head = head->next;
-            new_head->next_tail = head;
-            // Last tail must become emtpy.
-            last_tail->type = EMPTY;
-            last_tail->next_tail = nullptr;
-            break;
-        case 'a':
-            break;
-        case 'd':
-            break;
-        default:
-            break;
+    // Draw snake into the field.
+    for (int i = 0; i < length; i++) {
+        x = snake[i].posX;
+        y = snake[i].posY;
+        type = snake[i].type;
+        // Update field blocks.
+        arr[x][y] = type;
     }
-    return new_head;
 }
 
 int main(void) {
-    // todo make a pointer linked field
-
     // Init playing field.
-    struct block field[SIZE];
+    char field[SIZE][SIZE];
+    struct block snake[WIN_LENGTH];
+    int length = START_LENGTH;
 
-    // Init linked list.
-    int width = (int)sqrt(SIZE);
-    int x = 0;
-    int y = 0;
-
+    // Init field.
     for (int i = 0; i < SIZE; i++) {
-        if (i == 0) {
-            field[i].prev = nullptr;
-            field[i].next = &field[i+1];
-        } else if (i == SIZE - 1) {
-            field[i].next = nullptr;
-            field[i].prev = &field[i-1];
-        } else {
-            field[i].prev = &field[i-1];
-            field[i].next = &field[i+1];
+        for (int j = 0; j < SIZE; j++) {
+            field[i][j] = EMPTY;
         }
-        // All blocks are initially empty space.
-        field[i].type = EMPTY;
-        field[i].next_tail = nullptr;
-
-        if (x == width) {
-            y++;
-            x = 0;
-        }
-
-        field[i].posX = x;
-        field[i].posY = y;
-        x++;
     }
 
     // Init snake
-    struct block *head = &field[HEAD_POS];
-    head->type = HEAD;
-    head->next_tail = head->prev;
-
-    head->prev->type = BODY;
-    head->prev->next_tail = head->prev->prev;
-
-    head->prev->prev->type = BODY;
-    head->prev->prev->next_tail = head->prev->prev->prev;
-
-    head->prev->prev->prev->type = BODY;
-    head->prev->prev->prev->next_tail = head->prev->prev->prev->prev;
-
-    struct block *block = &field[0];
-    // We have a pointer to a struct so we need ->
-    // puts("Walk through the field:");
-    // while (block->next != NULL) {
-    //     printf("%d, Ptr: %p, Prev: %p, Next: %p, X: %d, Y: %d \n", block->type, block, block->prev, block->next, block->posX, block->posY);
-    //     block = block->next;
-    // }
-    // printf("%d, Ptr: %p, Prev: %p, Next: %p, X: %d, Y: %d \n", block->type, block, block->prev, block->next, block->posX, block->posY);
+    for (int i = 0; i < length; i++) {
+        snake[i].type = BODY;
+        snake[i].posX = HEAD_POS;
+        snake[i].posY = HEAD_POS + i;
+    }
 
     initscr();
     cbreak();
     noecho();
 
+    // Draw initial play area.
+    update_field(field, snake, length);
     print_field(field);
 
     char c = '\0';
     while (c != 'q') {
         scanf("%c", &c);
-        // todo update field and redraw.
-        head = update_field(head, c);
+        process_move(snake, c, length);
+        update_field(field, snake, length);
         print_field(field);
     }
 

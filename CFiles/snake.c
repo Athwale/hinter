16bd5bd2a570be507b71b900cfbb9dc57cef2625
Ondrej Mejzlik <omejzlik@redhat.com>
2025-12-17 16:20:50 +0100

    Prevent 180 turn

--- CFiles/snake.c
+++ CFiles/snake.c
@@ -3,12 +3,15 @@
 #include <math.h>
 #include <time.h>
 #include <stdlib.h>
+#include <poll.h>
+#include <unistd.h>
 
 // todo check sqrt and add if needed.
 #define SIZE 40
 #define HEAD_POS 20
 #define WIN_LENGTH 10
 #define START_LENGTH 3
+#define SPEED 200
 
 // block types:
 #define EMPTY '-'
@@ -35,9 +38,12 @@ void print_field(char arr[SIZE][SIZE]) {
     refresh();
 }
 
-void process_move(struct block snake[], char direction, int length) {
+bool process_move(struct block snake[], char direction, int length, int food_x, int food_y) {
     // Update the snake array with new coordinates.
     // todo implement boundaries.
+    // todo implement inability to 180 move
+    // todo implement winning
+    // todo implement biting yourself.
     // Take first element, back up coords, move it, put original coord to next element.
     int prev_x = 0;
     int prev_y = 0;
@@ -71,13 +77,14 @@ void process_move(struct block snake[], char direction, int length) {
             prev_y = current_y;
         }
     }
+    // Check if food was consumed
+    if (snake[0].posX == food_x && snake[0].posY == food_y) {
+        return true;
+    }
+    return false;
 }
 
-void update_field(char arr[SIZE][SIZE], struct block snake[], int length) {
-    int x = 0;
-    int y = 0;
-    char type = '\0';
-
+void update_field(char arr[SIZE][SIZE], struct block snake[], int length, int food_x, int food_y) {
     // Zero the whole field.
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
@@ -85,19 +92,29 @@ void update_field(char arr[SIZE][SIZE], struct block snake[], int length) {
         }
     }
 
+    // Place food.
+    arr[food_x][food_y] = FOOD;
+
     // Draw snake into the field.
     for (int i = 0; i < length; i++) {
-        x = snake[i].posX;
-        y = snake[i].posY;
-        type = snake[i].type;
-        // Update field blocks.
-        arr[x][y] = type;
+        arr[snake[i].posX][snake[i].posY] = snake[i].type;
     }
 }
 
+char get_input(char current) {
+    struct pollfd mypoll = {STDIN_FILENO, POLLIN|POLLPRI};
+    char c;
+    if( poll(&mypoll, 1, SPEED) )    {
+        scanf("%c", &c);
+        return c;
+    }
+    return current;
+}
+
 int main(void) {
+    // Init rand function.
     srand(time(nullptr));
-    // Init playing field.
+
     char field[SIZE][SIZE];
     struct block snake[WIN_LENGTH];
     int length = START_LENGTH;
@@ -118,21 +135,49 @@ int main(void) {
         snake[i].posY = HEAD_POS + i;
     }
 
+    // Init ncurses.
     initscr();
     cbreak();
     noecho();
 
     // Draw initial play area.
-    update_field(field, snake, length);
-    // Place food.
-    field[food_x][food_y] = FOOD;
+    update_field(field, snake, length, food_x, food_y);
     print_field(field);
 
-    char c = '\0';
+    // Main loop.
+    char last_direction = 's';
+    char c = 'w';
+    bool food_consumed = false;
+
     while (c != 'q') {
-        scanf("%c", &c);
-        process_move(snake, c, length);
-        update_field(field, snake, length);
+        c = get_input(c);
+        if (c == 'w' && last_direction == 's') {
+            c = 's';
+        }
+        if (c == 's' && last_direction == 'w') {
+            c = 'w';
+        }
+        if (c == 'a' && last_direction == 'd') {
+            c = 'd';
+        }
+        if (c == 'd' && last_direction == 'a') {
+            c = 'a';
+        }
+        last_direction = c;
+
+        food_consumed = process_move(snake, c, length, food_x, food_y);
+        if (food_consumed) {
+            // Grow snake. The new block will be added to the correct place on the next move.
+            snake[length].posX = snake[0].posX;
+            snake[length].posY = snake[0].posY;
+            snake[length].type = BODY;
+            length += 1;
+
+            // New food location.
+            food_x = rand() % SIZE;
+            food_y = rand() % SIZE;
+        }
+        update_field(field, snake, length, food_x, food_y);
         print_field(field);
     }
 

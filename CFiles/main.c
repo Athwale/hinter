17429bf17a3a6ba1489e181292984ceaa555e399
Ondrej Mejzlik <omejzlik@redhat.com>
2026-01-02 08:38:47 +0100

    Add labels

--- CFiles/main.c
+++ CFiles/main.c
@@ -11,7 +11,7 @@ static void activate (GtkApplication *app, gpointer user_data) {
     GtkWidget *button_box;
 
     window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "Window");
+    gtk_window_set_title(GTK_WINDOW(window), APP_NAME);
     gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
 
     button_box = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);

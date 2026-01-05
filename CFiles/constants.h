dda8beb2ac5f75dc1d3806ed5dcc90265b558fd3
Ondrej Mejzlik <omejzlik@redhat.com>
2026-01-05 14:20:32 +0100

    Add menu bar and text field.

--- CFiles/constants.h
+++ CFiles/constants.h
@@ -2,5 +2,5 @@
 
 #define APP_NAME "Writing aid"
 #define APP_INTERNAL_NAME "athwale.gtk.hinter"
-#define SIZE_H 200
-#define SIZE_W 300
+#define SIZE_H 800
+#define SIZE_W 1000
--- CFiles/main.c
+++ CFiles/main.c
@@ -18,29 +18,54 @@ static void activate (GtkApplication *app, gpointer user_data) {
     GtkWidget *box;
     GtkWidget *label;
     GtkWidget *text;
+    GtkWidget *field;
 
+    // Main window.
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), APP_NAME);
-    gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
+    gtk_window_set_default_size(GTK_WINDOW(window), SIZE_W, SIZE_H);
 
-    // Container.
-    box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 20);
+    // Single main container. Window can only have one widget, rest has to be inside.
+    box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
 
     // Label.
-    label = gtk_label_new("Label");
+    //label = gtk_label_new("Label");
 
     // Button.
-    button = gtk_button_new_with_label("Hello World");
+    //button = gtk_button_new_with_label("Hello World");
     // target, event, function, data.
-    g_signal_connect(button, "clicked", G_CALLBACK(b_click), label);
+    //g_signal_connect(button, "clicked", G_CALLBACK(b_click), label);
 
     // Text input.
-    text = gtk_entry_new();
+    //text = gtk_entry_new();
+
+    // Text field.
+    field = gtk_text_view_new();
+
+    // Menu bar. Is a separate vertical box,
+    GtkWidget *mbar = gtk_menu_bar_new ();
+    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    gtk_box_pack_start(GTK_BOX(vbox), mbar, TRUE, TRUE, 0);
+
+    GtkWidget *file_mi = gtk_menu_item_new_with_label("File");
+    gtk_menu_shell_append(GTK_MENU_SHELL(mbar), file_mi);
+
+    GtkWidget *f_menu = gtk_menu_new();
+    gtk_menu_item_set_submenu (GTK_MENU_ITEM(file_mi), f_menu);
+    GtkWidget *quit_mi = gtk_menu_item_new_with_label("Quit");
+    gtk_menu_shell_append (GTK_MENU_SHELL(f_menu), quit_mi);
+
+    g_signal_connect (quit_mi, "activate", G_CALLBACK(end_program),
+    NULL);
+
+    // Add menu box to the main box.
+    gtk_box_pack_start(GTK_BOX(box), vbox, FALSE, FALSE, 0);
 
     // Expand, fill
-    gtk_box_pack_start(GTK_BOX(box), text, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(box), label, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(box), button, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(box), field, TRUE, TRUE, 0);
+    //gtk_box_pack_start(GTK_BOX(box), text, TRUE, TRUE, 0);
+    //gtk_box_pack_start(GTK_BOX(box), label, TRUE, TRUE, 0);
+    //gtk_box_pack_start(GTK_BOX(box), button, FALSE, FALSE, 0);
 
     gtk_container_add(GTK_CONTAINER(window), box);
 
